9e9b3fa80773ba34e2502de3cd18edf16d245dfc
Young Kwon <youngkwon@openvpn-client.pcvpn.mediainsiders-panel.com>
2016-03-15 12:15:41 -0700

    added amount with income and expense cells

--- PayGeek/ViewController.swift
+++ PayGeek/ViewController.swift
@@ -154,7 +154,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             cell = tableView.dequeueReusableCellWithIdentifier("incomeOver", forIndexPath: indexPath)
             //let previewDetail = sampleData[indexPath.row]
             //cell!.textLabel!.text = previewDetail.title
-            cell!.textLabel!.text = MyIncome[indexPath.row].name
+            cell!.textLabel!.text = MyIncome[indexPath.row].name + ": $" + MyIncome[indexPath.row].amount
             
         }
         
@@ -162,7 +162,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             cell = tableView.dequeueReusableCellWithIdentifier("expenseRecent", forIndexPath: indexPath)
             //let previewDetail = sampleData1[indexPath.row]
             //cell!.textLabel!.text = previewDetail.title
-            cell!.textLabel!.text = MyExpense[indexPath.row].name
+            cell!.textLabel!.text = MyExpense[indexPath.row].name + ": $" + MyExpense[indexPath.row].amount
         }
         
         
